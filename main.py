4de7c88ffe55915ad75823fd22cbded51acc2c87
Jeferson <jefersonpeter32@gmail.com>
2021-08-07 03:35:10 -0300

    Script to scroll through a directory and delete old files

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+import datetime
+from tkinter import Tk
+from tkinter.filedialog import askdirectory
+
+if __name__ == '__main__':
+    Tk().withdraw()
+    path_to_file = askdirectory()
+    files = []
+    for dirpath, dirnames, filenames in os.walk(path_to_file):
+        for file in filenames:
+            curpath = os.path.join(dirpath, file)
+            file_modified = datetime.datetime.fromtimestamp(os.path.getmtime(curpath))
+            if datetime.datetime.now() - file_modified > datetime.timedelta(days=90): os.remove(curpath)
